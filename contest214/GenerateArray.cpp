9e1a93fd3ac077161f48cce3d329731d23262cb1
shashwat singh chauhan <textshashwat@gmail.com>
2020-11-08 11:05:54 +0530

    problem1

---
+++ contest214/GenerateArray.cpp
@@ -0,0 +1,32 @@
+/*
+LINK  
+https://leetcode.com/contest/weekly-contest-214/problems/get-maximum-in-generated-array/
+*/
+
+#include<iostream>
+#include<vector>
+#include<set>
+#include<map>
+#include<string>
+#include<cstring>
+//#include<bits/stdc++.h>
+using namespace std;
+ int getMaximumGenerated(int n){
+ 	if(n==0) return 0;
+ 	int num[n+1];
+ 	num[0]=0;
+ 	num[1]=1;
+ 	int m=1;
+ 	for(int i=2;i<=n;i++){
+ 		if(i%2==0)num[i]=num[i/2];
+ 		else num[i]=num[i/2]+num[(i/2)+1];
+ 		m=max(m,num[i]);
+ 	}
+ 	return m;
+
+ }
+int main(){
+	cout<<getMaximumGenerated(0);
+
+	return 0;
+}
